c9515b26ff3d02c00bbbbe67d975af7823a5797b
GhandiLou <hamonlouis1@gmail.com>
2025-06-12 21:58:15 +0100

    Create customPanel.js

---
+++ customPanel.js
@@ -0,0 +1,11 @@
+(() => {
+  const panel = document.getElementById('customPanel');
+  const panelBtn = document.getElementById('customPanelBtn');
+  const closeBtn = document.getElementById('closeCustomPanel');
+
+  // Inputs
+  const bgStart = document.getElementById('bgStartColor');
+  const bgMid = document.getElementById('bgMidColor');
+  const bgEnd = document.getElementById('bgEndColor');
+  const matrixSpeed = document.getElementById('matrixSpeedRange');
+  const
